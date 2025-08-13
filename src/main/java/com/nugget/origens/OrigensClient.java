50e570b5b24c16d3e1619da85438215c72b292c4
nugget-sk <nugget.sk.all@gmail.com>
2025-08-13 17:19:11 +0200

    Another Commit

--- src/main/java/com/nugget/origens/OrigensClient.java
+++ src/main/java/com/nugget/origens/OrigensClient.java
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = Origens.MODID, dist = Dist.CLIENT)
+@Mod(value = Origens.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = Origens.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = Origens.MOD_ID, value = Dist.CLIENT)
 public class OrigensClient {
     public OrigensClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
